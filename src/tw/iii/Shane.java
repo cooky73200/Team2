72f5123485dcc174b8daae53651cd909b4ae8636
Shane <sruangt@gmail.com>
2021-08-26 00:16:39 +0800

    upload

--- src/tw/iii/Shane.java
+++ src/tw/iii/Shane.java
@@ -5,6 +5,7 @@ public class Shane {
 	public static void main(String[] args) {
 		System.out.println("Polyphia");
 		System.out.println("Plini");
+		System.out.println("Ichika");
 		// TODO Auto-generated method stub
 
 	}
